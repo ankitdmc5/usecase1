0617f9b70b9214a38fb333869a36f4e047457960
unknown <ankitdmc5@gmail.com>
2021-04-20 22:16:26 +0530

    eureka service

--- company-service/src/main/java/com/ankitjain/company/service/CompanyServiceApplication.java
+++ company-service/src/main/java/com/ankitjain/company/service/CompanyServiceApplication.java
@@ -2,6 +2,7 @@ package com.ankitjain.company.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class CompanyServiceApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
